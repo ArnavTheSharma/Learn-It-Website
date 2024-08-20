2d49966fa8dd6ceb46466444519c31cfe35902e9
ArnavTheSharma <arnavsharma06@gmail.com>
2024-08-20 13:52:58 -0400

    Finished Section1 HomePage

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -1,11 +1,17 @@
 import "./HomePage.css";
+import { useNavigate } from "react-router-dom";
+
 
 function HomePage() {
+    const navigate = useNavigate();
+
     return (
        <>
         {/* Landing page */}
+        <div className="background"></div>
         <div className="homeContainer">
             <h1>Your Path to College and Career Success Starts <span>Here</span></h1>
+            <button className="joinUsButton" onClick={() => {navigate('/join-us');}}>Join US</button>
         </div>
 
         {/* About Learn It, certifications, Mission */}
