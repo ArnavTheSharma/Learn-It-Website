05c4a46e580478d4a5774ed025d1351dc3efbdf6
Codemeister14 <122707000+Codemeister14@users.noreply.github.com>
2024-08-22 16:07:58 -0700

    added links to emails

--- src/pages/teamPage/TeamPage.js
+++ src/pages/teamPage/TeamPage.js
@@ -9,17 +9,17 @@ function TeamPage() {
                 <div className="teamMembers">
                     <img src="" alt="image"></img>
                     <h3>Piyusha Majgaonkar</h3>
-                    <h4>piyusha.majgaonkar@gmail.com</h4>
+                    <h4><a href="mailto:piyusha.majgaonkar@gmail.com">piyusha.majgaonkar@gmail.com</a></h4>
                 </div>
                 <div className="teamMembers">
                     <img src="" alt="image"></img>
                     <h3>Aryan Das</h3>
-                    <h4>itsaryandas15@gmail.com</h4>
+                    <h4><a href="mailto:itsaryandas15@gmail.com">itsaryandas15@gmail.com</a></h4>
                 </div>
                 <div className="teamMembers">
                     <img src="" alt="image"></img>
                     <h3>Sunaina Pal</h3>
-                    <h4>sunainapal2006@gmail.com</h4>
+                    <h4><a href="mailto:sunainapal2006@gmail.com">sunainapal2006@gmail.com</a></h4>
                 </div>
             </div>
             <p id="aboutTeam">Hi Everyone, we are the Co-Founders of Learn It! We are seniors from Massachusettes and we've always had a passion for learning and making a difference in our community. What better way to do that than to spread joy through our program? We hope you join us on this journey and learn a little something along the way!</p>
@@ -29,7 +29,7 @@ function TeamPage() {
                 <div id="socialMediaPic">
                     <img src="" alt="image"></img>
                     <h3>Himal Bokka</h3>
-                    <h4>Himalkumar2020@gmail.com</h4>
+                    <h4><a href="mailto:Himalkumar2020@gmail.com">Himalkumar2020@gmail.com</a></h4>
                 </div>
                 <p id="socialText">Meet Himal Bokka, the  Social Media Manager here at Learn It. He is the design manager for the rocketry club at his high school and is part of the social media team at his local library. Himal loves to play sports like football, basketball, and soccer in his free time. Himal is willing to help out Learn It on the social media side of the organization and wants to contribute more to the Learn It group.</p>
             </div>
@@ -39,7 +39,7 @@ function TeamPage() {
                 <div id="commPic">
                     <img src="" alt="image"></img>
                     <h3>Merav Sriram</h3>
-                    <h4>meravsriram@gmail.com</h4>
+                    <h4><a href="mailto:meravsriram@gmail.com">meravsriram@gmail.com</a></h4>
                 </div>
                 <p id="commText">Merav Sriram, a rising junior in high school, is the new Communications Coordinator at Learn It Academics. He has made multiple appearances on TV show PL Fanzone, which features soccer enthusiasts as they compete in challenges and enjoys interviewing international soccer stars. Aside from his extensive theoretical knowledge, Merav enjoys competitive soccer, chess and debate, and is a part of NEFC. Merav is excited to contribute to the Learn It team and hopes to organize fun and effective events!</p>
             </div>
