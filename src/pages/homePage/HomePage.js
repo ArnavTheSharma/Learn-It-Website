5d8475ff5873ebbec3872dc2832e6a0a640b5190
ArnavTheSharma <arnavsharma06@gmail.com>
2024-08-23 19:15:51 -0400

    Minor Changes 3

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -1,6 +1,5 @@
 import "./HomePage.css";
 import { useNavigate } from "react-router-dom";
-import Section2 from "./section2/section2.js";
 import Section3 from "./section3/Section3";
 
 
@@ -8,20 +7,17 @@ function HomePage() {
     const navigate = useNavigate();
 
     return (
-        <>
+       <>
         {/* Landing page */}
+        <div className="background"></div>
         <div className="homeContainer">
             <h1>Your Path to College and Career Success Starts <span>Here</span></h1>
             <button className="joinUsButton" onClick={() => {navigate('/join-us');}}>Join US</button>
         </div>
-        {/* About Learn It, certifications, Mission */}
-        <section>
 
-        </section>
         {/* About Learn It, certifications, Mission */}
-        <section>
-
-        </section>
+        {/*  Section with 23 partners, volunteer count, webinar count */}
+        <Section3 />
         {/* Join Us and Contact Us Page Redirects  */}
         {/* Upcoming Events Section (Preview)  */}
         {/* Partners Section  */}
@@ -29,6 +25,7 @@ function HomePage() {
                 partners
         </section>
         </>
+
     );
 }
 
