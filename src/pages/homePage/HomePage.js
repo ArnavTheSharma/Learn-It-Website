146abc016cdd9776a127cf89bdaac3b2e640df59
Beri28 <cletusberinyuy@gmail.com>
2024-08-23 14:48:57 +0100

    minor updates

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -24,12 +24,46 @@ function HomePage() {
             <Section3 />
         </section>
         {/* Join Us and Contact Us Page Redirects  */}
-        <section>
+        <section className="my-6">
+        earn It is a proud 501(c)(3) certified non-profit organization working to bridge the STEM
+education accessibility gap by providing free STEM education and career-exploration
+resources for all. Learn It recently gained a Youth-Led Non-Profit Organization Impact
+Certification from CMHQ. This, in turn, means that Learn It will be expanding even more
+internationally and will be launching more initiatives that will require more volunteers.
+● We, here at Learn It, want to create a difference in our community and spread our
+mutual joy for learning. We are working towards providing free extracurricular education
+and tutoring services for youth around the country who wish to seek higher education in
+STEM. This program aims to offer support and informational resources for students while
+providing opportunities to gain volunteer hours for our members. We truly believe that
+the first step to success is opening the door to knowledge.
+● We are a student-run organization whose goal is to aid our student community by
+providing FREE, informative, and educational college prep, career, and academic
+resources. We host regular webinars regarding various college prep and career
+exploration topics, and also offer various volunteer opportunities for our members.
 
         </section>
         {/* Upcoming Events Section (Preview)  */}
-        <section>
+        <section className="my-6">
+        earn It is a proud 501(c)(3) certified non-profit organization working to bridge the STEM
+education accessibility gap by providing free STEM education and career-exploration
+resources for all. Learn It recently gained a Youth-Led Non-Profit Organization Impact
+Certification from CMHQ. This, in turn, means that Learn It will be expanding even more
+internationally and will be launching more initiatives that will require more volunteers.
+● We, here at Learn It, want to create a difference in our community and spread our
+mutual joy for learning. We are working towards providing free extracurricular education
+and tutoring services for youth around the country who wish to seek higher education in
+STEM. This program aims to offer support and informational resources for students while
+providing opportunities to gain volunteer hours for our members. We truly believe that
+the first step to success is opening the door to knowledge.
+● We are a student-run organization whose goal is to aid our student community by
+providing FREE, informative, and educational college prep, career, and academic
+resources. We host regular webinars regarding various college prep and career
+exploration topics, and also offer various volunteer opportunities for our members.
 
+        </section>
+        {/* Partners Section  */}
+        <section id="partners" >
+                partners
         </section>
         </>
 
--- src/pages/homePage/sections/section3/Section3.js
+++ src/pages/homePage/sections/section3/Section3.js
@@ -7,15 +7,20 @@ const Section3 = () => {
     const [volunteers,setVolunteers]=useState(2)
     const [partners,setPartners]=useState(23)
     const [webinars,setWebinars]=useState(3)
+    const handlePartnersClick=()=>{
+        const target=document.getElementById('partners')
+        target.scrollIntoView({behavior:'smooth'})
+        // window.scrollTo()
+    }
     return ( 
         <div className="section3 flex justify-center items-center py-10">
             <div className=" flex justify-center items-center border-[.3em] border-white">
-                <div className="bg-[#9890aa] p-20 flex justify-center items-center">
-                    <div className='flex flex-col justify-center items-center'>
-                        <FaHandshake className='rotate-90 text-red-300 w-full text-3xl' />
-                        <p className='text-4xl font-bold mr-0'>{partners}</p>
-                        <p className='text-sm font-bold mr-0'>Partners</p>
-                    </div>
+                <div className="bg-[#9890aa] p-20 flex justify-center items-center" onClick={()=>handlePartnersClick()}>
+                        <div className='flex flex-col justify-center items-center'>
+                            <FaHandshake className='rotate-90 text-red-300 w-full text-3xl' />
+                            <p className='text-4xl font-bold mr-0'>{partners}</p>
+                            <p className='text-sm font-bold mr-0'>Partners</p>
+                        </div>
                 </div>
                 <div className="bg-white p-20 flex justify-center items-center">
                     <div className='flex flex-col justify-center items-center'>
