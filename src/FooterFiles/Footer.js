50a222aa38420ff6c9873bf000a0260b66598b90
Codemeister14 <122707000+Codemeister14@users.noreply.github.com>
2024-08-23 21:04:12 -0700

    styling

--- src/FooterFiles/Footer.js
+++ src/FooterFiles/Footer.js
@@ -7,11 +7,12 @@ function Footer(){
         <div id="Footer">
             <div id="quickLinks">
                 <div id="socialLinks">
-                    <a href="https://www.facebook.com/groups/276584458322427/"><img src={facebook}></img></a>
-                    <a href="https://www.instagram.com/learn.it_academics/"><img src={insta}></img></a>
-                    <a href="https://discord.com/invite/cD9645WNYr"><img src={discord}></img></a>
+                    <a href="https://www.facebook.com/groups/276584458322427/"><img src={facebook} className="image"></img></a>
+                    <a href="https://www.instagram.com/learn.it_academics/"><img src={insta} className="image"></img></a>
+                    <a href="https://discord.com/invite/cD9645WNYr"><img src={discord} className="image"></img></a>
                 </div>
             </div>
+            <hr></hr>
             <p id="copyright">&copy;2024 by Learn It</p>
         </div>
     );
